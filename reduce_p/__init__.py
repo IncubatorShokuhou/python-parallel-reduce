8eb98c7e8db8a9b7ebfad75828e03a5c099c6027
IncubatorShokuhou <lh@lasg.iap.ac.cn>
2020-11-24 15:17:47 +0800

    0.0.2

--- reduce_p/__init__.py
+++ reduce_p/__init__.py
@@ -1,6 +1,19 @@
+import more_itertools as mit
+from itertools import repeat
+from functools import reduce,wraps
+import multiprocessing
 __version__ = '0.0.2'
-__author__ ='Hao Lyu'
-from functools import wraps
+
+
+def reduce_p(function, sequence, n_jobs=multiprocessing.cpu_count(), initial=None):
+    if n_jobs == 1:
+        return reduce(function, sequence, initial)
+    else:
+        sliced_sequence = mit.distribute(n_jobs, sequence)
+        with multiprocessing.Pool(n_jobs) as pool:
+            new_sequence = pool.starmap(reduce, zip(
+                repeat(function), sliced_sequence, repeat(initial)))
+        return reduce(function, new_sequence, initial)
 
 def handle_nan(function):
     @wraps(function)
